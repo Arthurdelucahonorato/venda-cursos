80ec717eae157dd017f9b3724cc6a883d567d8c5
Guilherme Brito Pizzollo <86429792+guilhermebp030504@users.noreply.github.com>
2024-11-19 20:31:44 -0300

    Update Video.ts

--- src2.0/composite/Video.ts
+++ src2.0/composite/Video.ts
@@ -9,6 +9,24 @@ export class Video implements CursoComponente {
         this.componentes.push(componente);
     }
 
+    editarComponente(indice: number, novoComponente: CursoComponente): void {
+        if (indice >= 0 && indice < this.componentes.length) {
+            this.componentes[indice] = novoComponente;
+            console.log('Vídeo do vídeo editado com sucesso.');
+        } else {
+            console.error('Índice inválido.');
+        }
+    }
+
+    removerComponente(indice: number): void {
+        if (indice >= 0 && indice < this.componentes.length) {
+            this.componentes.splice(indice, 1);
+            console.log('Vídeo do vídeo removido com sucesso.');
+        } else {
+            console.error('Índice inválido.');
+        }
+    }
+
     exibir(nivel: number = 0): void {
         console.log(`${' '.repeat(nivel * 2)}Vídeo: ${this.titulo}`);
         this.componentes.forEach(componente => componente.exibir(nivel + 1)); // Incrementa o nível para subcomponentes
