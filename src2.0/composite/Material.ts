7bf4a2896c6993cfa77aff323d8c7f0354cf23cc
Guilherme Brito Pizzollo <86429792+guilhermebp030504@users.noreply.github.com>
2024-11-19 20:31:26 -0300

    Update Material.ts

--- src2.0/composite/Material.ts
+++ src2.0/composite/Material.ts
@@ -9,8 +9,25 @@ export class Material implements CursoComponente {
         this.componentes.push(componente);
     }
 
+    editarComponente(indice: number, novoComponente: CursoComponente): void {
+        if (indice >= 0 && indice < this.componentes.length) {
+            this.componentes[indice] = novoComponente;
+            console.log('Material editado com sucesso.');
+        } else {
+            console.error('Índice inválido.');
+        }
+    }
+
+    removerComponente(indice: number): void {
+        if (indice >= 0 && indice < this.componentes.length) {
+            this.componentes.splice(indice, 1);
+            console.log('Material removido com sucesso.');
+        } else {
+            console.error('Índice inválido.');
+        }
+    }
+
     exibir(nivel: number = 0): void {  // Adicionando valor padrão para 'nivel'
-        let nive = 0
         console.log(`${' '.repeat(nivel * 2)}Material: ${this.titulo}`);
         this.componentes.forEach(componente => componente.exibir(nivel + 1)); // Passando o valor do nível incrementado
     }
